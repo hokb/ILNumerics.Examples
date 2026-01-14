34bd856ce6216d7e50ba800cb61dd4cdfc8d4f5d
hokb <h.kutschbach@ilnumerics.net>
2026-01-14 11:54:45 +0100

    Initial: zoom factor example (WinForms)

---
+++ ZoomFactorExample/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ZoomFactorExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ilPanel1 = new ILNumerics.Drawing.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.zoomOutButton = new System.Windows.Forms.Button();
+            this.zoomInButton = new System.Windows.Forms.Button();
+            this.buttonShowAll = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ilPanel1
+            // 
+            this.ilPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ilPanel1.RendererType = ILNumerics.Drawing.RendererTypes.OpenGL;
+            this.ilPanel1.Editor = null;
+            this.ilPanel1.Location = new System.Drawing.Point(84, 0);
+            this.ilPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ilPanel1.Name = "ilPanel1";
+            this.ilPanel1.Rectangle = ((System.Drawing.RectangleF)(resources.GetObject("ilPanel1.Rectangle")));
+            this.ilPanel1.ShowUIControls = false;
+            this.ilPanel1.Size = new System.Drawing.Size(615, 522);
+            this.ilPanel1.TabIndex = 0;
+            this.ilPanel1.Timeout = ((uint)(0u));
+            this.ilPanel1.Load += new System.EventHandler(this.ilPanel1_Load);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.buttonShowAll);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.zoomOutButton);
+            this.panel1.Controls.Add(this.zoomInButton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(79, 467);
+            this.panel1.TabIndex = 1;
+            // 
+            // zoomOutButton
+            // 
+            this.zoomOutButton.Location = new System.Drawing.Point(3, 93);
+            this.zoomOutButton.Name = "zoomOutButton";
+            this.zoomOutButton.Size = new System.Drawing.Size(68, 81);
+            this.zoomOutButton.TabIndex = 1;
+            this.zoomOutButton.Text = "Zoom Out";
+            this.zoomOutButton.UseVisualStyleBackColor = true;
+            this.zoomOutButton.Click += new System.EventHandler(this.zoomOutButton_Click);
+            // 
+            // zoomInButton
+            // 
+            this.zoomInButton.Location = new System.Drawing.Point(4, 4);
+            this.zoomInButton.Name = "zoomInButton";
+            this.zoomInButton.Size = new System.Drawing.Size(68, 81);
+            this.zoomInButton.TabIndex = 0;
+            this.zoomInButton.Text = "Zoom In";
+            this.zoomInButton.UseVisualStyleBackColor = true;
+            this.zoomInButton.Click += new System.EventHandler(this.zoomInButton_Click);
+            // 
+            // buttonShowAll
+            // 
+            this.buttonShowAll.Location = new System.Drawing.Point(3, 180);
+            this.buttonShowAll.Name = "buttonShowAll";
+            this.buttonShowAll.Size = new System.Drawing.Size(68, 81);
+            this.buttonShowAll.TabIndex = 1;
+            this.buttonShowAll.Text = "Show All";
+            this.buttonShowAll.UseVisualStyleBackColor = true;
+            this.buttonShowAll.Click += new System.EventHandler(this.buttonShowAll_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zoom Out";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.zoomOutButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 522);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ilPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ILNumerics.Drawing.Panel ilPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button zoomOutButton;
+        private System.Windows.Forms.Button zoomInButton;
+        private System.Windows.Forms.Button buttonShowAll;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ZoomFactorExample/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ILNumerics;
+using ILNumerics.Drawing;
+
+namespace ZoomFactorExample
+{
+    /// <summary>
+    /// This examples demonstrates how to apply different zoom settings to an ILCamera based scene. 
+    /// It allows the user to zoom by mouse wheel as well as with help of two buttons attached to the form.  
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ilPanel1_Load(object sender, EventArgs e)
+        {
+
+            // setup the scene
+            ilPanel1.Scene.Camera.Add(new Cylinder());
+
+            // apply further configuration to the scene. This does not affect the zooming as such but is included here for 
+            // demonstration purposes only. 
+            ilPanel1.Scene.Camera.Projection = Projection.Orthographic;
+            ilPanel1.Scene.Camera.Add(new Tripod());
+
+        }
+        // the button handlers are registered in the forms designer. This handles clicks on the ZoomIn Button
+        private void zoomInButton_Click(object sender, EventArgs e)
+        {
+            // zoom in
+            doZoomStepFirstCamera(true);
+        }
+
+        // Button handler for zoom out button
+        private void zoomOutButton_Click(object sender, EventArgs e)
+        {
+            // zoom out
+            doZoomStepFirstCamera(false);
+        }
+
+        // this method implements the actual zoom operation. It is called by the button click handlers.
+        private void doZoomStepFirstCamera(bool zoomin)
+        {
+            // get the current zoom factor: fetch it from the camera in the synchronized scene. This is the 
+            // one which is individually maintained by each panel (driver) and getting altered by the mouse, 
+            // independently from the global scene. See: http://ilnumerics.net/scene-management.html
+            var camera = ilPanel1.SceneSyncRoot.First<Camera>();
+            if (camera != null) {
+
+                // update the camera zoom factor by adding the update value
+                // refer to: http://ilnumerics.net/apidoc/html/P_ILNumerics_Drawing_ILCamera_ZoomFactor.htm
+                camera.ZoomFactor += (zoomin ? -.1 : .1);
+                // trigger immediate redraw
+                ilPanel1.Refresh();
+            }
+        }
+
+        private void buttonShowAll_Click(object sender, EventArgs e) {
+            // get the camera relevant for interactions 
+            var camera = ilPanel1.SceneSyncRoot.First<Camera>();
+            if (camera != null) {
+                // call the extension method on the camera
+                camera.ShowAll(); 
+                // trigger immediate redraw
+                ilPanel1.Refresh();
+            }
+        }
+    }
+    public static class Extensions {
+        /// <summary>
+        /// Extension method to set the zoom factor of a camera object to show all content (orthographic zoom only)
+        /// </summary>
+        /// <param name="camera"></param>
+        public static void ShowAll(this Camera cam) {
+            // this example works only with orthographic projection. You will need to take the view frustum 
+            // into account, if you want to make this method work with perspective projection also. however, 
+            // the general functioning would be similar....
+            if (cam.Projection != Projection.Orthographic) {
+                throw new NotImplementedException();
+            }
+            // get the overall extend of the cameras scene content
+            var limits = cam.GetLimits();
+            // take the maximum of width/ height
+            var maxExt = limits.HeightF > limits.WidthF ? limits.HeightF : limits.WidthF;
+            // make sure the camera looks at the unrotated bounding box
+            cam.Reset(); 
+            // center the camera view
+            cam.LookAt = limits.CenterF;
+            // apply the zoom factor: the zoom factor will scale the 'left', 'top', 'bottom', 'right' limits 
+            // of the view. In order to fit exactly, we must take the "radius"
+            cam.ZoomFactor = maxExt * .50;
+
+        }
+
+    }
+
+}
