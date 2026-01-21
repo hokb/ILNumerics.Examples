d551f400a1a159d987f7fdb12400cc244c41aa62
hokb <h.kutschbach@ilnumerics.net>
2026-01-21 09:24:15 +0100

    Visual Basic minimal surface example added

---
+++ VisualBasic_MiniSurfaceExample/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports ILNumerics.ILMath
+Imports ILNumerics.Globals
+Imports ILNumerics
+Imports ILNumerics.Drawing.Plotting
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim myPanel = New ILNumerics.Drawing.Controls.WinForms.Panel
+        Controls.Add(myPanel)
+        myPanel.Dock = DockStyle.Fill
+
+        'some data 
+        Dim A As Array(Of Single) = tosingle(SpecialData.terrain(r(0, 50), r(0, 50)))
+        Dim pc = myPanel.Scene.Add(New PlotCube(twoDMode:=False))
+        Dim surf = pc.Add(New Surface(A))
+        myPanel.Configure()
+
+    End Sub
+End Class
